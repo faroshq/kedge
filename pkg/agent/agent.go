d3ed31e26969235184265cccaf3d0ef459907a0f
Mangirdas Judeikis <mangirdas@judeikis.lt>
2026-03-12 19:33:26 +0200

    lint

--- pkg/agent/agent.go
+++ pkg/agent/agent.go
@@ -34,7 +34,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/apimachinery/pkg/types"
-	"k8s.io/apimachinery/pkg/runtime/schema"
 	"k8s.io/client-go/dynamic"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"

cd971f499c16d3fa8f1e0e05182d900e4ff975d5
Mangirdas Judeikis <mangirdas@judeikis.lt>
2026-03-23 13:33:29 +0100

    embed graphql

--- pkg/hub/options.go
+++ pkg/hub/options.go
@@ -62,7 +62,7 @@ func NewOptions() *Options {
 		DataDir:             "/tmp/kedge-data",
 		ListenAddr:          ":9443",
 		HubExternalURL:      "https://localhost:9443",
-		GraphQLAddr:         "localhost:9090",
+		GraphQLAddr:         "",
 		EmbeddedKCP:         false,
 		KCPSecurePort:       6443,
 		KCPBindAddress:      "127.0.0.1",
